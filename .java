279ebb8806261be66af95a5e8593f8af7ee7eeeb
Sunedo <crisomsan537@gmail.com>
2014-03-06 19:15:06 +0800

    modify
    modify the display

--- .java
+++ .java
@@ -5,35 +5,44 @@ publi class TestFunctions{
     Scanner input = new Scanner(System.in);
     
     System.out.println("There are seven function here:\na. f(x) = x^2;\nb. f(x) = sin(x);\nc. f(x) = cos(x;\nd. f(x) = tan(x);\ne. f(x) = cos(x) + 5sin(x);\nf. f(x) = 5cos(x) + sin(x);\ng. f(x) = log(x) + x^2)");
+    
     System.out.println("Enter the number you want to calculate function a.");
     double a1 = input.nextDouble();
-    System.out.println("The answer to function a is:" +  );
-   
-    System.out.println("Enter the number you want to calculate function b.");
+    
+    System.out.println("Enter the number  x you want to calculate function b.");
     double b1 = input.nextDouble();
-    System.out.println("The answer to function b is:" + );
-   
-    System.out.println("Enter the number you want to calculate function c.");
+  
+    System.out.println("Enter the number x you want to calculate function c.");
     double c1 = input.nextDouble();
-    System.out.println("The answer to function c is:" +  );
-    
-    System.out.println("Enter the number you want to calculate function d.");
+
+    System.out.println("Enter the number x you want to calculate function d.");
     double d1 = input.nextDouble();
-    System.out.println("The answer to function d is:" +  );
-   
-    System.out.println("Enter the number you want to calculate function e.");
+    
+    System.out.println("Enter the number x you want to calculate function e.");
     double e1 = input.nextDouble();
-    System.out.println("The answer to function e is:" +  );
     
-    System.out.println("Enter the number you want to calculate function f.");
+    System.out.println("Enter the number x you want to calculate function f.");
     double f1 = input.nextDouble();
-    System.out.println("The answer to function f is:" +  );
     
-    System.out.println("Enter the number you want to calculate function g.");
+    System.out.println("Enter the number x you want to calculate function g.");
     double g1 = input.nextDouble();    
-    System.out.println("The answer to function g is:" +  );
     
     Functions answer = new Functions(a1, b1, c1, d1, e1, f1, g1);
+    
+    System.out.println("The answer to function a is:" +  );
+    
+    System.out.println("The answer to function b is:" + );
+    
+    System.out.println("The answer to function c is:" +  );
+    
+    System.out.println("The answer to function d is:" +  );
+    
+    System.out.println("The answer to function e is:" +  );
+    
+    System.out.println("The answer to function f is:" +  );
+    
+    System.out.println("The answer to function g is:" +  );
+    
     }
 }
 
@@ -41,15 +50,51 @@ publi class TestFunctions{
     Function(){
     }
     
-    Function(a1, b1, c1, d1, e1, f1, g1){
-      a1 = a1;
-      b1 = b1;
-      c1 = c1;
-      d1 = d1;
-      e1 = e1;
-      f1 = f1;
-      g1 = g1;
+    double a2 = 0;
+    double b2 = 0;
+    double c2 = 0;
+    double d2 = 0;
+    double e2 = 0;
+    double f2 = 0;
+    double g2 = 0;
+    
+    
+    Function( double a1, double b1, double c1, double d1, double e1, double f1, double g1){
+      a2 = a1;
+      b2 = b1;
+      c2 = c1;
+      d2 = d1;
+      e2 = e1;
+      f2 = f1;
+      g2 = g1;
     }
     
+    double getFunctiona(){
+      return (Math.pow(a2, 2));
+    }
     
+    double getFunctionb(){
+      return (Math.sin(b2));
+    }
+    
+    double getFunctionc(){
+      return (Math.cos(c2));
+    }
+    
+    double getFunctiond(){
+      return (Math.tan(d2))
+    }
+
+    double getFunctione(){
+      
+    }
+    
+    double getFunctionf(){
+      
+    }    
+    
+    double getFunctiong(){
+      
+    }
+  
   }
