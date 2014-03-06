2ab98426d93a355c7bb802c4b7702aa7201a7665
Sunedo <crisomsan537@gmail.com>
2014-03-06 19:01:22 +0800

    delete some number and use class to solution
    create the class function to calculate

--- .java
+++ .java
@@ -4,42 +4,52 @@ publi class TestFunctions{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
     
-    double a.result = 0;
-    double b.result = 0;
-    double c.result = 0;
-    double d.result = 0;
-    double e.result = 0;
-    double f.result = 0;
-    double g.result = 0;
-    
     System.out.println("There are seven function here:\na. f(x) = x^2;\nb. f(x) = sin(x);\nc. f(x) = cos(x;\nd. f(x) = tan(x);\ne. f(x) = cos(x) + 5sin(x);\nf. f(x) = 5cos(x) + sin(x);\ng. f(x) = log(x) + x^2)");
-    System.out.println("Enter the integer you want to caculate function a.");
+    System.out.println("Enter the number you want to calculate function a.");
     double a1 = input.nextDouble();
-    System.out.println("The answer to function a is:" +  a.result);
+    System.out.println("The answer to function a is:" +  );
    
-    System.out.println("Enter the integer you want to caculate function b.");
+    System.out.println("Enter the number you want to calculate function b.");
     double b1 = input.nextDouble();
-    System.out.println("The answer to function b is:" +  b.result);
+    System.out.println("The answer to function b is:" + );
    
-    System.out.println("Enter the integer you want to caculate function c.");
+    System.out.println("Enter the number you want to calculate function c.");
     double c1 = input.nextDouble();
-    System.out.println("The answer to function c is:" +  c.result);
+    System.out.println("The answer to function c is:" +  );
     
-    System.out.println("Enter the integer you want to caculate function d.");
+    System.out.println("Enter the number you want to calculate function d.");
     double d1 = input.nextDouble();
-    System.out.println("The answer to function d is:" +  d.result);
+    System.out.println("The answer to function d is:" +  );
    
-    System.out.println("Enter the integer you want to caculate function e.");
+    System.out.println("Enter the number you want to calculate function e.");
     double e1 = input.nextDouble();
-    System.out.println("The answer to function e is:" +  e.result);
+    System.out.println("The answer to function e is:" +  );
     
-    System.out.println("Enter the integer you want to caculate function f.");
+    System.out.println("Enter the number you want to calculate function f.");
     double f1 = input.nextDouble();
-    System.out.println("The answer to function f is:" +  f.result);
+    System.out.println("The answer to function f is:" +  );
     
-    System.out.println("Enter the integer you want to caculate function g.");
+    System.out.println("Enter the number you want to calculate function g.");
     double g1 = input.nextDouble();    
-    System.out.println("The answer to function g is:" +  g.result);
+    System.out.println("The answer to function g is:" +  );
+    
+    Functions answer = new Functions(a1, b1, c1, d1, e1, f1, g1);
     }
-  
 }
+
+  class Functions{
+    Function(){
+    }
+    
+    Function(a1, b1, c1, d1, e1, f1, g1){
+      a1 = a1;
+      b1 = b1;
+      c1 = c1;
+      d1 = d1;
+      e1 = e1;
+      f1 = f1;
+      g1 = g1;
+    }
+    
+    
+  }
