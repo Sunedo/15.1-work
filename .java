d3629643ef17069848f17c2c408067dd5b203668
Sunedo <crisomsan537@gmail.com>
2014-03-06 18:46:57 +0800

    Write the main class
    test class prompt user enter the number and display

---
+++ .java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+publi class TestFunctions{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    
+    double a.result = 0;
+    double b.result = 0;
+    double c.result = 0;
+    double d.result = 0;
+    double e.result = 0;
+    double f.result = 0;
+    double g.result = 0;
+    
+    System.out.println("There are seven function here:\na. f(x) = x^2;\nb. f(x) = sin(x);\nc. f(x) = cos(x;\nd. f(x) = tan(x);\ne. f(x) = cos(x) + 5sin(x);\nf. f(x) = 5cos(x) + sin(x);\ng. f(x) = log(x) + x^2)");
+    System.out.println("Enter the integer you want to caculate function a.");
+    double a1 = input.nextDouble();
+    System.out.println("The answer to function a is:" +  a.result);
+   
+    System.out.println("Enter the integer you want to caculate function b.");
+    double b1 = input.nextDouble();
+    System.out.println("The answer to function b is:" +  b.result);
+   
+    System.out.println("Enter the integer you want to caculate function c.");
+    double c1 = input.nextDouble();
+    System.out.println("The answer to function c is:" +  c.result);
+    
+    System.out.println("Enter the integer you want to caculate function d.");
+    double d1 = input.nextDouble();
+    System.out.println("The answer to function d is:" +  d.result);
+   
+    System.out.println("Enter the integer you want to caculate function e.");
+    double e1 = input.nextDouble();
+    System.out.println("The answer to function e is:" +  e.result);
+    
+    System.out.println("Enter the integer you want to caculate function f.");
+    double f1 = input.nextDouble();
+    System.out.println("The answer to function f is:" +  f.result);
+    
+    System.out.println("Enter the integer you want to caculate function g.");
+    double g1 = input.nextDouble();    
+    System.out.println("The answer to function g is:" +  g.result);
+    }
+  
+}
